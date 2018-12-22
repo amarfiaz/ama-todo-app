9231435975da79df5c67c65c4fe3ccdf9aac8ea3
Mohammed Amar Fiaz (Financial Services) <mohammed-amar.fiaz@lloydsbanking.com>
2018-12-22 21:38:54 +0000

    setup Redux

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = configureStore();
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
